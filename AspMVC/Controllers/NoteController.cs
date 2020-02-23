6467ebdeeccc1935df7ce4661556ab0c4c917f23
soo.toance <soo.toance@gmail.com>
2020-02-23 22:03:27 +0900

    게시물 관리 Controller 추가
    - 리스트, 추가, 편집, 삭제 함수 추가

---
+++ AspMVC/Controllers/NoteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        /// <summary>
+        /// 게시판 리스트
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 추가 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 수정 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 게시물 삭제 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
