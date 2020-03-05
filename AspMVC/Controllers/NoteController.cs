43b0f8046f901c78a47f4d4e6d638ff71b77a2f4
soo.toance <soo.toance@gmail.com>
2020-03-05 23:15:00 +0900

    게시물 로그인 검증 및 사용자 ID 기능 연동

--- AspMVC/Controllers/NoteController.cs
+++ AspMVC/Controllers/NoteController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AspMVC.DataContext;
 using System.Linq;
+using AspMVC.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace AspMVC.Controllers
 {
@@ -12,6 +14,12 @@ namespace AspMVC.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
             using (var db = new AspNoteDbContext())
             {
                 var list = db.Notes.ToList(); // 모두 출력 
@@ -25,12 +33,25 @@ namespace AspMVC.Controllers
         /// <returns></returns>
         public IActionResult Add()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(Note model)
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                // 로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+
             if (ModelState.IsValid)
             {
                 using (var db = new AspNoteDbContext())
